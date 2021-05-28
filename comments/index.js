d694a551f6006031aa1a1a4774e5200ce965966b
Marnus Coetzee <marnus@specno.com>
2021-05-28 16:21:15 +0200

    finish comments service

--- comments/index.js
+++ comments/index.js
@@ -10,6 +10,29 @@ app.use(
   })
 );
 
-app.listen(4200, () => {
-  console.log("Listening on Port 4200 :)");
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({
+    id: commentId,
+    content,
+  });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log("Listening on Port 4001 :)");
 });
